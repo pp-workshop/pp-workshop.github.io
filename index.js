00ced93c6f7c294cc785e07b73f683d8287051a6
Przemysław Sulecki <psugrg@gmail.com>
2024-03-01 22:59:22 +0100

    multiply quiz with styles

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-let ViewnameElm = document.getElementById("viewname");
 let MultiplyAnchorElm = document.getElementById("multiplyAnchor");
-
-ViewnameElm.textContent = "PP-workshop";
 MultiplyAnchorElm.textContent = "Tabliczka";
